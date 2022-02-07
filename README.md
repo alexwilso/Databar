bf707f7c93396d3d1d90d00e5f2a8b0baaa30f5a
Alexander Wilson <wilsoal9@flip1.engr.oregonstate.edu>
2022-02-06 18:35:16 -0800

    Adding html for employees, events, jobs

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # databar
 CS 340 Group Project
+
+
+To run:
+cd into directory
+run command: npm install
+run command: node index.js [port #]
---
+++ index.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var app = express();
+var handlebars = require("express-handlebars").create({defaultLayout:"main"});
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+app.set('port', process.argv[2]);
+app.use(express.static("public"));
+
+// Home Page
+app.get("/", (req, res, next) => {
+    var context = {};
+    res.render("home", context);
+  });
+
+// Employees Page
+app.get("/employees", (req, res, next) => {
+var context = {};
+res.render("employee", context);
+});
+
+// Events Page
+app.get("/events", (req, res, next) => {
+	var context = {};
+	res.render("events", context);
+	});
+
+// Jobs Page
+app.get("/jobs", (req, res, next) => {
+	var context = {};
+	res.render("jobs", context);
+	});
+
+// 404 not found
+app.use((req,res) => { 
+	res.status(404);
+	res.render('404');
+});
+
+// 500 server error
+app.use((err,  req,  res,  next) => {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+// listen
+app.listen(app.get('port'), () => {
+	console.log(`Server is listening on http://${process.env.HOSTNAME}:${app.get('port')}; press Ctrl-C to terminte` ); 
+});
