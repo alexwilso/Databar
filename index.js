85449adfbedfa9b17d268ec56c32c56e9888a790
Dub1250 <71575246+Dub1250@users.noreply.github.com>
2022-02-10 02:21:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var express = require("express");
 var app = express();
 var handlebars = require("express-handlebars").create({defaultLayout:"main"});
@@ -35,6 +36,18 @@ app.get("/jobs", (req, res, next) => {
 	res.render("jobs", context);
 	});
 
+// Menu Page
+app.get("/menu", (req, res, next) => {
+	var context = {};
+	res.render("menu", context);
+	});
+
+// Inventory Page
+app.get("/inventory", (req, res, next) => {
+	var context = {};
+	res.render("inventory", context);
+	});
+
 // 404 not found
 app.use((req,res) => { 
 	res.status(404);
@@ -51,4 +64,77 @@ app.use((err,  req,  res,  next) => {
 // listen
 app.listen(app.get('port'), () => {
 	console.log(`Server is listening on http://${process.env.HOSTNAME}:${app.get('port')}; press Ctrl-C to terminte` ); 
+=======
+var express = require("express");
+var app = express();
+var handlebars = require("express-handlebars").create({defaultLayout:"main"});
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+app.set('port', process.argv[2]);
+app.use(express.static("public"));
+
+// Home Page
+app.get("/", (req, res, next) => {
+    var context = {};
+    res.render("home", context);
+  });
+
+// Employees Page
+app.get("/employees", (req, res, next) => {
+var context = {};
+res.render("employee", context);
+});
+
+// Events Page
+app.get("/events", (req, res, next) => {
+	var context = {};
+	res.render("events", context);
+	});
+
+// Jobs Page
+app.get("/jobs", (req, res, next) => {
+	var context = {};
+	res.render("jobs", context);
+	});
+
+// Menu Page
+app.get("/menu", (req, res, next) => {
+	var context = {};
+	res.render("menu", context);
+	});
+
+// Inventory Page
+app.get("/inventory", (req, res, next) => {
+	var context = {};
+	res.render("inventory", context);
+	});
+
+
+
+
+
+
+
+// 404 not found
+app.use((req,res) => { 
+	res.status(404);
+	res.render('404');
+});
+
+// 500 server error
+app.use((err,  req,  res,  next) => {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+// listen
+app.listen(app.get('port'), () => {
+	console.log(`Server is listening on http://${process.env.HOSTNAME}:${app.get('port')}; press Ctrl-C to terminte` ); 
+>>>>>>> 26266bace38062510963f1110220bfc5fbce45ae
 });
