8ea8665f79c3aa297333b80401f251bb34a38364
Alexander Wilson <wilsoal9@flip1.engr.oregonstate.edu>
2022-02-27 15:42:45 -0800

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var handlebars = require("express-handlebars").create({defaultLayout:"main"});
 var bodyParser = require("body-parser");
+var mysql = require("./dbcon.js");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -11,42 +12,88 @@ app.set("view engine", "handlebars");
 app.set('port', process.argv[2]);
 app.use(express.static("public"));
 
+
 // Imports images
 app.use(express.static('public'));
 
 // Home Page
 app.get("/", (req, res, next) => {
-    var context = {};
+    let context = {};
     res.render("home", context);
   });
 
 // Employees Page
 app.get("/employees", (req, res, next) => {
-	var context = {};
+	let selectEmployees = 'SELECT * FROM Employees';
+	mysql.pool.query(selectEmployees, function(err, rows, fields) {
+		if (err) {
+			console.log(err);
+		} else {
+			
+		};
+
+	});
+
+	let context = {};
 	res.render("employee", context);
 });
 
 // Events Page
 app.get("/events", (req, res, next) => {
-	var context = {};
+	let selectEvents = 'SELECT * FROM Events';
+	mysql.pool.query(selectEvents, function(err, rows, fields) {
+		if (err) {
+			console.log(err);
+		} else {
+			
+		};
+	});
+
+	let context = {};
 	res.render("events", context);
 	});
 
 // Jobs Page
 app.get("/jobs", (req, res, next) => {
-	var context = {};
+	let selectJobs = 'SELECT * FROM Jobs';
+	mysql.pool.query(selectJobs, function(err, rows, fields) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Successful');
+			
+		};
+	});
+	let context = {};
 	res.render("jobs", context);
 	});
 
 // Menu Page
 app.get("/menu", (req, res, next) => {
-	var context = {};
+	let selectMenu = 'SELECT * FROM Drinks';
+	mysql.pool.query(selectMenu, function(err, rows, fields) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Successful');
+		};
+	});
+
+	let context = {};
 	res.render("menu", context);
 	});
 
 // Inventory Page
 app.get("/inventory", (req, res, next) => {
-	var context = {};
+	let selectInventory = 'SELECT * FROM Inventory';
+	mysql.pool.query(selectInventory, function(err, rows, fields){
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Successful');
+		};
+	});
+	let context = {};
 	res.render("inventory", context);
 	});
 
