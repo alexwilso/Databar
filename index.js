604b88372129d3e38428ac6fd6c417dab3fa0d73
Ethan Ruark <ruarke@flip2.engr.oregonstate.edu>
2022-03-02 21:46:47 -0800

    Added Menu Insert

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ let handlebars = require("express-handlebars").create({
 });
 let bodyParser = require("body-parser");
 let mysql = require("./database/dbcon.js");
-let router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -75,21 +74,6 @@ app.get("/employees", (req, res, next) => {
 });
 // Insert Employee
 app.post('/employees', (req, res) => {
-	var sql = "INSERT INTO Employees (first_name, last_name, telephone, job_code, start_date) VALUES (?,?,?,?,?)";
-	var inserts = [req.body.first_name, req.body.last_name, req.body.telephone, req.body.job_code, req.body.start_date];
-	sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
-		if(error){
-			res.write(JSON.stringify(error));
-			res.end();
-		} else {
-			console.log('Employee added');
-			res.redirect('/employees');
-		}
-	});
-});
-
-router.post('/employees', (req, res) => {
-	var mysql = req.app.get('mysql');
 	var sql = "INSERT INTO Employees (first_name, last_name, telephone, job_code, start_date) VALUES (?,?,?,?,?)";
 	var inserts = [req.body.first_name, req.body.last_name, req.body.telephone, req.body.job_code, req.body.start_date];
 	sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
@@ -171,7 +155,6 @@ app.get("/jobs", (req, res, next) => {
 
 // Insert Job
 app.post('/jobs', (req, res) => {
-	// var mysql = req.app.get('mysql');
 	var sql = "INSERT INTO Jobs (job_title, hourly_rate) VALUES (?,?)";
 	var inserts = [req.body.job_title, req.body.hourly_rate];
 	sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
@@ -208,6 +191,22 @@ app.get("/menu", (req, res, next) => {
 				};
 			});
 		};
+	});
+});
+
+// Insert Menu Item
+app.post('/menu', (req, res) => {
+	// var mysql = req.app.get('mysql');
+	var sql = "INSERT INTO Drinks (drink_name, ingredient_1, ingredient_2, ingredient_3, ingredient_4, ingredient_5, price) VALUES (?,?,?,?,?,?,?)";
+	var inserts = [req.body.drink_name, req.body.ingredient_1, req.body.ingredient_2, req.body.ingredient_3, req.body.ingredient_4, req.body.ingredient_5, req.body.price];
+	sql = mysql.pool.query(sql, inserts, function(error, results, fields) {
+		if(error){
+			res.write(JSON.stringify(error));
+			res.end();
+		} else {
+			console.log('Menu Item added');
+			res.redirect('/menu');
+		}
 	});
 });
 
